8b0f768589c18506e9603d1f1300b96fb1013295
johnmichaelespiritu <johnmichael.espiritu.pixel8@gmail.com>
2023-10-07 00:05:00 +0800

    Implement a while loop to get the value of the parameters.

--- memory_check.sh
+++ memory_check.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+# Initialize variables
+critical_threshold=""
+warning_threshold=""
+email_address=""
+
+# Parse command-line parameters using getopts
+while getopts "c:w:e:" option;
+do
+	case "$option" in
+		c) critical_threshold="$OPTARG" ;;
+		w) warning_threshold="$OPTARG" ;;
+		e) email_address="$OPTARG" ;;
+		*) echo "Invalid option: -$option" >&2
+		   exit 1 ;;
+	esac
+done
+
+echo $critical_threshold
+echo $warning_threshold
+echo $email_address
+
